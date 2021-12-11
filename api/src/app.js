75d03a1134c6e76b84765b8fdd82fc1fdef6812b
marharita08 <72861258+marharita08@users.noreply.github.com>
2021-12-11 18:57:49 +0200

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 
 app.get("/", function (req, res) {
-  res.send("Hello World" + process.env.PORT);
+  res.send("Hello World");
 });
 
 app.listen(process.env.PORT || 3000);
