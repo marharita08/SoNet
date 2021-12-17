2b10e2d4d3224143e218a936211183bb6cf0717e
marharita08 <rozgon.margarita08@gmail.com>
2021-12-17 19:44:30 +0200

    api-task2

--- api/src/services/config.js
+++ api/src/services/config.js
@@ -1,3 +1,8 @@
 module.exports = {
   appPort: process.env.APP_PORT,
+  dbHost: process.env.DB_HOST,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbDatabase: process.env.DB_DATABASE,
+  dbPort: process.env.DB_PORT,
 };
