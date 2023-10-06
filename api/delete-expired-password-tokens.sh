b82a4014536ea47ef6582fbeadb63158bbe70d04
marharita08 <rozgon.margarita08@gmail.com>
2023-10-06 11:29:18 +0300

    fix cron environment issue

--- api/delete-expired-password-tokens.sh
+++ api/delete-expired-password-tokens.sh
@@ -1,1 +1,3 @@
+echo "$(date): Running delete-expired-password-tokens.sh" >> /var/log/cron.log
+exec >> /var/log/cron.log 2>&1
 psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE -c "DELETE FROM reset_password_tokens WHERE expires_on < NOW();"
