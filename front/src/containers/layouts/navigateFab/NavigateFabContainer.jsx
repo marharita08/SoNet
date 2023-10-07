5fbd628239daeb14ad62902b22424c93facef8d9
marharita08 <rozgon.margarita08@gmail.com>
2023-10-07 16:27:14 +0300

    add scroll to top button

---
+++ front/src/containers/layouts/navigateFab/NavigateFabContainer.jsx
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from "react";
+import NavigateFab from "../../../components/atoms/iconButtons/NavigateFab";
+
+const NavigateFabContainer = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 150) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
+    return (
+        <>
+            {
+                isVisible && <NavigateFab onClick={scrollToTop}/>
+            }
+        </>
+    );
+};
+
+export default NavigateFabContainer;
