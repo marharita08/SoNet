cf27d4c46a780f2034314382528e71ff384cdf84
marharita08 <rozgon.margarita08@gmail.com>
2024-02-03 13:33:52 +0200

    update universities storage

--- api/src/db/universities/storage.js
+++ api/src/db/universities/storage.js
@@ -1,21 +1,32 @@
 const db = require("../../configs/db");
-const {tables, shortColumns} = require("../dbSchema");
+const BaseStorage = require("../base/storage");
 
-module.exports = {
-    getAll: () =>
-        db
-            .select(
-                `${shortColumns.universities.universityId} as value`,
-                `${shortColumns.universities.name} as label`
-            )
-            .from(tables.universities)
-            .orderBy(shortColumns.universities.name),
-    getByName: (name) =>
-        db.select().from(tables.universities).where(shortColumns.universities.name, name),
-    create: (university) => db(tables.universities).insert(university).returning(shortColumns.universities.universityId),
-    getRandomUniversityId: () =>
-        db(tables.universities)
-            .select(shortColumns.universities.universityId).first()
-            .orderByRaw("random()")
-            .limit(1)
-};
+class UniversitiesStorage extends BaseStorage {
+  constructor() {
+    super("universities", "university_id", db);
+  }
+
+  getAll = async () =>
+    this.db
+      .select(
+        `${this.primaryKey} as value`,
+        "name as label"
+      )
+      .from(this.table)
+      .orderBy("name");
+
+  getByName = async (name) =>
+    this.db
+      .select()
+      .from(this.table)
+      .where("name", name);
+
+  getRandomUniversityId = async () =>
+    db(this.table)
+      .select(this.primaryKey)
+      .first()
+      .orderByRaw("random()")
+      .limit(1);
+}
+
+module.exports = new UniversitiesStorage();
