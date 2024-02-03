6c2ebf89d18316b1fc0d6ee91b02be8cc96b4145
marharita08 <rozgon.margarita08@gmail.com>
2024-02-03 12:44:05 +0200

    update comments storage

--- api/src/db/base/storage.js
+++ api/src/db/base/storage.js
@@ -1,18 +1,34 @@
 class BaseStorage {
-    constructor(table, primaryKey, db) {
-        this.table = table;
-        this.db = db;
-        this.primaryKey = primaryKey;
-    }
-    getAll = async () => this.db(this.table).select();
+  constructor(table, primaryKey, db) {
+    this.table = table;
+    this.db = db;
+    this.primaryKey = primaryKey;
+  }
 
-    getById = async (id) => this.db(this.table).select().where(this.primaryKey, id);
+  getAll = async () =>
+    this.db(this.table)
+      .select();
 
-    create = async (entity) => this.db(this.table).returning(this.primaryKey).insert(entity);
+  getById = async (id) =>
+    this.db(this.table)
+      .select()
+      .first()
+      .where(this.primaryKey, id);
 
-    update = async (id, entity) => this.db(this.table).update(entity).where(this.primaryKey, id);
+  create = async (entity) =>
+    this.db(this.table)
+      .returning(this.primaryKey)
+      .insert(entity);
 
-    delete = async (id) => this.db(this.table).delete().where(this.primaryKey, id);
+  update = async (id, entity) =>
+    this.db(this.table)
+      .update(entity)
+      .where(this.primaryKey, id);
+
+  delete = async (id) =>
+    this.db(this.table)
+      .delete()
+      .where(this.primaryKey, id);
 }
 
 module.exports = BaseStorage;
