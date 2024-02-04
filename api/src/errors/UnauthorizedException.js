3a3d5ab1d4547f6505086a12b313fcbefd9fec1f
marharita08 <rozgon.margarita08@gmail.com>
2024-02-04 17:41:30 +0200

    update error files

--- api/src/errors/UnauthorizedException.js
+++ api/src/errors/UnauthorizedException.js
@@ -3,11 +3,11 @@ const Messages = require("../constants/messages");
 const {Name, Code} = require("../constants/exceptions");
 
 class UnauthorizedException extends ClientException {
-    constructor(message) {
-        super(message || Messages.UNAUTHORIZED);
-        this.name = Name.UNAUTHORIZED;
-        this.statusCode = Code.UNAUTHORIZED;
-    }
+  constructor(message) {
+    super(message || Messages.UNAUTHORIZED);
+    this.name = Name.UNAUTHORIZED;
+    this.statusCode = Code.UNAUTHORIZED;
+  }
 }
 
 module.exports = UnauthorizedException;
